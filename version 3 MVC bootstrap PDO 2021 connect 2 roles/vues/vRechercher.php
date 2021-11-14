8f0a749159f79c73289cde038ffc7d905ac20ff2
NoaSillam <noasillam@gmail.com>
2021-11-14 13:34:42 +0100

    modificationCvEtArchitecture

---
+++ version 3 MVC bootstrap PDO 2021 connect 2 roles/vues/vRechercher.php
@@ -0,0 +1,15 @@
+
+<!--Saisie des informations dans un formulaire!-->
+<div class="container">
+
+<form name="formAjout" action="" method="post" onSubmit="return valider()">
+  <fieldset>
+    <legend>Entrez la d�signation de la fleur recherch�e </legend>
+   <label>D�signation :</label> <input type="text" name="des" size="20" /><br />
+  </fieldset>
+  <button type="submit" class="btn btn-primary">Rechercher</button>
+  </p>
+</form>
+</div>
+
+
